1ca4c842444e278452d9a0280120866b3338aa43
Codecambing <h.olivaresrifo@gmail.com>
2026-01-16 18:49:51 -0300

    Deploy configurado con Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://codecambing.github.io',
+  base: '/TDM-webpage',
   vite: {
     plugins: [tailwindcss()]
+    
   }
 });
